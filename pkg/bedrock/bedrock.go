01504c3f696e5cccdb62f74354db76acd77a4057
Alexander Gorenkov <alex@grnkv.ru>
2022-05-16 17:43:12 +0400

    Disconnecting after receiving statistics

--- pkg/bedrock/bedrock.go
+++ pkg/bedrock/bedrock.go
@@ -1,10 +1,5 @@
 package bedrock
 
-import (
-	"fmt"
-	"os"
-)
-
 // Stat returns the status of the minecraft:bedrock server
 func Stat(hostname string, port int) (BEStat, error) {
 	client := NewClient(hostname, port)
@@ -14,14 +9,12 @@ func Stat(hostname string, port int) (BEStat, error) {
 		return BEStat{}, err
 	}
 
-	defer func() {
-		err = client.Disconnect()
-		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, "Disconnect error: ", err.Error())
-		}
-	}()
-
 	stat, err := client.Stat()
+	if err != nil {
+		return BEStat{}, err
+	}
+
+	err = client.Disconnect()
 	if err != nil {
 		return BEStat{}, err
 	}
